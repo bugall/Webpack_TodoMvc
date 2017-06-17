2f5abeecf4414b76ba164504adbf99ed06b39c06
bugall <bugall@sina.cn>
2017-06-17 17:16:00 +0800

    [wip]

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import 'babel-polyfill';
 import cats from './cats';
 import $ from 'jquery';
 
+const name = "bugall";
+
+console.log(`./folder/${name}/xxx`)
 $('<h1>Cats</h1>').appendTo('body');
 const ul = $('<ul></ul>').appendTo('body');
 for (const cat of cats) {
